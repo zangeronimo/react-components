a96b944ee42d1c822b2cc2201fa2928d0931bbd1
Luciano Zangeronimo <zangeronimo@gmail.com>
2022-06-22 18:53:22 -0300

    feat: button componeent

--- src/components/atoms/button/index.tsx
+++ src/components/atoms/button/index.tsx
@@ -1,4 +1,30 @@
-const Button = () => {
-  return <button type="button">One Button</button>
+import { Sizes, Skins } from '@/components/utils/types'
+import { HTMLAttributes, ReactNode } from 'react'
+
+import Styles from './button.module.scss'
+
+type Props = HTMLAttributes<HTMLButtonElement> & {
+  skin?: Skins
+  outline?: boolean
+  full?: boolean
+  size?: Sizes
+  children: ReactNode
+}
+
+const Button = ({
+  skin = 'primary',
+  outline = false,
+  full = false,
+  size = 'normal',
+  children,
+  ...rest
+}: Props) => {
+  const bg = outline ? `outline_${skin}` : skin
+  const styles = `${Styles[bg]} ${Styles[size]} ${full ? Styles.full : ''}`
+  return (
+    <button type="button" className={styles} {...rest}>
+      {children}
+    </button>
+  )
 }
 export default Button
