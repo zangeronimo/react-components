1e9b5e8f63e69e0ae552b42b573f72405a62f8af
Luciano Zangeronimo <zangeronimo@gmail.com>
2022-06-25 19:36:24 -0300

    refactor: styles adjustments

--- src/components/atoms/group/index.tsx
+++ src/components/atoms/group/index.tsx
@@ -6,13 +6,22 @@ import Styles from './group.module.scss'
 type Props = {
   align?: Aligns
   nowrap?: boolean
+  column?: boolean
   children: ReactNode
 }
 
-const Group = ({ align = 'right', nowrap = false, children }: Props) => {
-  const styles = `${Styles.container} ${Styles[align]} ${
-    nowrap ? Styles.nowrap : ''
-  }`
+const Group = ({
+  align = 'right',
+  column = false,
+  nowrap = false,
+  children,
+}: Props) => {
+  const styles = `
+    ${Styles.container}
+    ${Styles[align]}
+    ${nowrap ? Styles.nowrap : ''}
+    ${column ? Styles.column : ''}
+  `
   return <div className={styles}>{children}</div>
 }
 
